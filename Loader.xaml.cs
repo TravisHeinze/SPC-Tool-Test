ff3b448151e662083d4dc73e5523e379ad7b7cb3
TravisHeinze <heinze.travis@gmail.com>
2021-07-28 10:58:46 -0700

    Added function to add local driver to tool if driver is missing.

--- Loader.xaml.cs
+++ Loader.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,20 +13,49 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Data.Odbc;
+using Microsoft.Win32;
 
 namespace SPC_Tool
 {
     /// <summary>
     /// Interaction logic for Window1.xaml
     /// </summary>
+    /// 
+
     public partial class Loader : Window
     {
         public Loader(DataTable SPCLimits, DataTable SPCData)
         {
+            VerifyDriver();
             InitializeComponent();
             this.Show();
             GetTablesODBC(SPCLimits, SPCData);
             this.Close();
+        }
+
+        public void VerifyDriver()
+        {
+            bool driverCheck = false;
+
+            RegistryKey reg = Registry.CurrentUser.OpenSubKey("Software\\ODBC\\ODBC.INI\\ODBC Data Sources\\");
+
+            if (reg != null)
+            {
+                foreach (string name in reg.GetValueNames())
+                {
+                    if (reg.GetValue(name).ToString() == "Microsoft Access Driver (*.mdb, *.accdb)")
+                    {
+                        driverCheck = true;
+                    }
+                }
+                reg.Close();
+            }
+            
+            if(driverCheck == false)
+            {
+                CreateDSN();
+            }
+
         }
 
         public void GetTablesODBC(DataTable SPCLimits, DataTable SPCData)
@@ -77,7 +107,39 @@ namespace SPC_Tool
                 MessageBox.Show("Failed to connect to database. Error " + ex.ToString());
                 this.Close();
             }
+        }
+
+        public void CreateDSN()
+        {
+            string ODBC_PATH = "SOFTWARE\\ODBC\\ODBC.INI\\";
+            string driverName = "Microsoft Access Driver (*.mdb, *.accdb)";
+            string dsnName = "Test Driver";
+            string description = "This DSN was created from code!";
+
+            string driverPath = @"C:\Program Files\Microsoft Office\root\vfs\ProgramFilesCommonX64\Microsoft Shared\OFFICE16\ACEODBC.DLL";
+
+            // value to odbc data source         
+            var datasourcesKey = Registry.CurrentUser.CreateSubKey(ODBC_PATH + "ODBC Data Sources");
+
+            if (datasourcesKey == null)
+            {
+                throw new Exception("ODBC Registry key does not exist!!");
+            }
+
+            datasourcesKey.SetValue(dsnName, driverName);
+
+            //It will Create new key in odbc.ini         
+            var dsnKey = Registry.CurrentUser.CreateSubKey(ODBC_PATH + dsnName);
+
+            if (dsnKey == null)
+            {
+                throw new Exception("ODBC Registry key not created!!");
+            }
+
+            dsnKey.SetValue("Description", description);
+            dsnKey.SetValue("Driver", driverPath);
 
         }
     }
 }
+
